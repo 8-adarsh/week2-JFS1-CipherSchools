a6ae92a1f64088612267f503638304c69bbcde77
ADARSH KUMAR LAL <80323632+8-adarsh@users.noreply.github.com>
2023-02-05 00:11:05 +0530

    Add files via upload

---
+++ test3.java
@@ -0,0 +1,18 @@
+class father{
+    public final String lastName ="oberoi";
+    public final void foodHabit(){
+        System.out.println("i like salty!!");
+}
+}
+
+class son extends father{
+//public void foodHabit(){
+// lastname ="Malhotra";
+///System.out.println("i like sweet");
+//}
+
+ public class test3{
+public static void main( String[] args){
+}
+}
+}
